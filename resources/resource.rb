39bc81548e5a14ef99bf3438b632515fea0db963
Yuta Miyama Okazaki <s04155yo@gmail.com>
2016-11-12 11:43:58 +0900

    WIP

--- resources/resource.rb
+++ resources/resource.rb
@@ -8,13 +8,13 @@ class ResourceMap
     @map[key] += vol
   end
 
-  def enough_resource_for?(animal)
-    animal.required_resource_cnt <= cnt_map_for(animal)
+  def enough_resource_for?(animal, discount: 0)
+    (animal.required_resource_cnt - discount) <= cnt_map_for(animal)
   end
 
-  def subtract_for(animal)
-    raise "Not Enough Resource" if !enough_resource_for?(animal)
-    @map[animal.resource_color] -= animal.required_resource_cnt
+  def subtract_for(animal, discount: 0)
+    @map[animal.resource_color] -= (animal.required_resource_cnt - discount)
+    @map[animal.resource_color] = 0 if @map[animal.resource_color] < 0
   end
 
   def to_s
